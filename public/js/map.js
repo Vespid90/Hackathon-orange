815e57d676edad9926e16e321f841d133f958bdc
Mohab G <77176417+houba1337@users.noreply.github.com>
2024-12-04 11:06:29 +0100

    Updated map logic

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,6 @@ const map = L.map('map').setView([50.411461, 4.44424], 15);
 
 let navigation = document.getElementById('map');
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
-//     {foo: 'bar',
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
