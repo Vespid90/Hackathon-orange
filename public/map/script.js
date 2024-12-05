c5ae8a31d80e4dfdca3937a2bf9612ec21d01f60
KikiMaouw <cristelle.hermand@gmail.com>
2024-12-05 15:05:46 +0100

    📦 NEW: close icon image

--- public/map/script.js
+++ public/map/script.js
@@ -11,3 +11,37 @@ const addUserLoc = document.getElementById('add_user_loc');
 addUserButton.addEventListener('click', () => {
     addUserLoc.style.display = 'block';
 });
+
+const CloseButtonZone = document.getElementById('close_button_zone');
+const CloseZoneLoc = document.getElementById('add_zone_loc');
+
+CloseButtonZone.addEventListener('click', () => {
+    CloseZoneLoc.style.display = 'none';
+});
+
+const CloseButtonUser = document.getElementById('close_button_user');
+const CloseUserLoc = document.getElementById('add_user_loc');
+
+CloseButtonUser.addEventListener('click', () => {
+    CloseUserLoc.style.display = 'none';
+});
+
+
+const users = [
+    { id: 1, name: 'Maxime' },
+    { id: 2, name: 'Farid' },
+    { id: 3, name: 'Cristelle' },
+    { id: 4, name: 'Mohab' },
+    { id: 5, name: 'Hugo' }
+];
+
+const userSelect = document.getElementById('user_select');
+
+userSelect.innerHTML = '';
+
+users.forEach(user => {
+    const option = document.createElement('option');
+    option.value = user.id;
+    option.textContent = user.name;
+    userSelect.appendChild(option);
+});
