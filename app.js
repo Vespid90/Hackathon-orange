55652ff3cda8f387d7b9a6fd806a46c765bd9bf7
Mxmac <pro.maximeandre@gmail.com>
2024-12-05 14:33:25 +0100

    Import et utilisation de cors

--- app.js
+++ app.js
@@ -2,26 +2,28 @@ import express from 'express';
 import locationRoutes from './routes/LocationRoutes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import cors from 'cors'
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:63342',
+}));
 
 
-// Configuration pour gérer JSON et formulaires
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir les fichiers HTML et assets statiques
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Utilisation des routes
+
 app.use('/api', locationRoutes);
 
-// Lancement du serveur
+
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
